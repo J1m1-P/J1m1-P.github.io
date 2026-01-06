09fce560730ad552f4cf89f01576eeb5f6aca2c6
Jimmy Pan <jimmypan0410@gmail.com>
2026-01-06 00:14:45 -0800

    init setup and ready for HeroText and Layout

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+    return (
+        <main>
+            <div>
+                <h1 className="text-3xl underline">
+                    Welcome to my page!
+                </h1>
+            </div>
+        </main>
+    )
+}
+
+export default App
